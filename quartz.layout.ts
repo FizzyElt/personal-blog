94766a0f7b8af741e33acc98ccee9282d5cfbd73
FizzyElt <fizzyelt.dev@outlook.com>
2025-02-12 02:11:29 +0800

    Quartz sync: Feb 12, 2025, 2:11 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,5 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
+import { SimpleSlug } from "./quartz/util/path"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -27,23 +28,35 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer({
-      folderClickBehavior: "link",
-      sortFn: (a, b) => {
-        if (!a.file && !b.file) {
-          return 0
-        }
-
-        if (a.file?.frontmatter?.published && b.file?.frontmatter?.published) {
-          const dateA = new Date(a.file.frontmatter.published)
-          const dateB = new Date(b.file.frontmatter.published)
-
-          return dateB.getTime() - dateA.getTime()
-        }
-
-        return a.file && !b.file ? 1 : -1
-      },
+    Component.RecentNotes({
+      limit: 3,
+      showTags: false,
+      linkToMore: "/notes" as SimpleSlug,
+      filter: (f) => !!f.relativePath?.startsWith("notes/"),
     }),
+    Component.RecentNotes({
+      limit: 3,
+      showTags: false,
+      linkToMore: "/posts" as SimpleSlug,
+      filter: (f) => !!f.relativePath?.startsWith("posts/"),
+    }),
+    // Component.Explorer({
+    //   folderClickBehavior: "link",
+    //   sortFn: (a, b) => {
+    //     if (!a.file && !b.file) {
+    //       return 0
+    //     }
+
+    //     if (a.file?.frontmatter?.published && b.file?.frontmatter?.published) {
+    //       const dateA = new Date(a.file.frontmatter.published)
+    //       const dateB = new Date(b.file.frontmatter.published)
+
+    //       return dateB.getTime() - dateA.getTime()
+    //     }
+
+    //     return a.file && !b.file ? 1 : -1
+    //   },
+    // }),
   ],
   right: [
     Component.Graph(),
@@ -60,7 +73,6 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.Explorer(),
   ],
   right: [],
 }
