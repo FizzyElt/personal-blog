9f80ef21327670b2a3c814882fee4d39a7d1080f
FizzyElt <fizzyelt8786@gmail.com>
2026-06-22 22:21:17 +0800

    setup base config

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,14 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as Plugin from "./.quartz/plugins"
+
+Plugin.RecentNotes({
+  title: "Recent Notes/Posts",
+  limit: 5,
+  showTags: false,
+  linkToMore: false,
+  filter: (f: any) =>
+    !!f.relativePath?.startsWith("notes/") || !!f.relativePath?.startsWith("posts/"),
+})
 
 const config = await loadQuartzConfig()
 export default config
